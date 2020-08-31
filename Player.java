b1fd4e02ca39c0057ec293bea052b6fb35cf5ee6
Elijah Guarina <50428456+ZeusExMachina@users.noreply.github.com>
2020-08-31 16:15:37 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,84 +1,162 @@
-import java.awt.*;
+import java.util.*;
 
-/**
- * This Location class holds all the information about each
- * square on the Cludeo board.
- *
- * @author Jared Boult
- */
-public class Location {
-    Room room;
-    boolean northWall;
-    boolean eastWall;
-    boolean southWall;
-    boolean westWall;
-    Point point;
-    boolean occupied;
-    Piece piece;
+public class Player {
+	private final Map<String, Card> hand;
+	private final int playerNumber;
+	private final String playerName;
+	private final String characterName;
+	private final Random rand;
+	public Game g;
+	private boolean canAccuse;
+	private int counter;
+	private Stack<Location> prevLocations;
+	private Set<Location> locationsVisited;
 
-    public Location(Room room, String walls, int boardRow, int boardColumn){
-        this.room = room;
-        this.northWall = walls.contains("N");
-        this.eastWall = walls.contains("E");
-        this.southWall = walls.contains("S");
-        this.westWall = walls.contains("W");
-        this.point = new Point(boardColumn, boardRow);
-    }
+	public Player(int playerNum, String username, String character, Game game) {
+		this.hand = new HashMap<>();
+		this.playerNumber = playerNum;
+		this.playerName = username;
+		this.characterName = character;
+		this.rand = new Random();
+		this.prevLocations = new Stack<Location>();
+		this.locationsVisited = new HashSet<Location>();
+		this.g = game;
+		this.canAccuse = true;
+	}
 
-    public Room getRoom() {
-        return room;
-    }
+	/**
+	 * Returns the number of this player (e.g. Player "1")
+	 * @return Integer
+	 */
+	public int getPlayerNumber() { return playerNumber; }
 
-    public Piece getPiece() {
-        return piece;
-    }
+	/**
+	 * Returns the username of this Player
+	 * @return String
+	 */
+	public String getPlayerName() { return playerName; }
 
-    public boolean canMoveUp(Board board){
-        if(point.y <= 0){
-            return false;
-        }
-        boolean squareIsOccupied = board.currentBoard[point.y-1][point.x].occupied;
-        return !northWall && !squareIsOccupied;
-    }
+	/**
+	 * Returns the name of the character associated with this Player
+	 * @return String
+	 */
+	public String getCharacterName() { return characterName; }
 
-    public boolean canMoveDown(Board board){
-        if(point.y >= Board.HEIGHT-1){
-            return false;
-        }
-        boolean squareIsOccupied = board.currentBoard[point.y+1][point.x].occupied;
-        return !southWall && !squareIsOccupied;
-    }
+	public Map<String,Card> getHand(){
+		return hand;
+	}
 
-    public boolean canMoveRight(Board board){
-        if(point.x >= Board.WIDTH-1){
-            return false;
-        }
-        boolean squareIsOccupied = board.currentBoard[point.y][point.x+1].occupied;
-        return !eastWall && !squareIsOccupied;
-    }
+	public boolean canAccuse() { return canAccuse; }
 
-    public boolean canMoveLeft(Board board){
-        if(point.x <= 0){
-            return false;
-        }
-        boolean squareIsOccupied = board.currentBoard[point.y][point.x-1].occupied;
-        return !westWall && !squareIsOccupied;
-    }
-    
-    public String checkAdjacent(Location other){
-        if(this.point.x == other.point.x && this.point.y - other.point.y == 1){
-            return "W";
-        }
-        if(this.point.x - other.point.x == 1 && this.point.y == other.point.y){
-            return "A";
-        }
-        if(this.point.x == other.point.x && this.point.y - other.point.y == -1){
-            return "S";
-        }
-        if(this.point.x - other.point.x == -1 && this.point.y == other.point.y){
-            return "D";
-        }
-        return "Invalid";
-    }
-    
+	public void setCannotAccuse() { canAccuse = false; }
+
+	/**
+	 * Puts Card into players hand
+	 * @param card
+	 */
+	public void giveCard(Card card) { hand.put(card.getName(),card); }
+
+	public Integer prepareForMove(){
+		locationsVisited.clear();
+		prevLocations.clear();
+		counter = rollDice();
+		return counter;
+	}
+
+	/**
+	 * Roll two dice and get their sum. Two numbers are generated
+	 * to try to mimic real dice.
+	 *
+	 * @return the total of the two dice
+	 */
+	private Integer rollDice() {
+		int first = rand.nextInt(6) + 1, second = rand.nextInt(6) + 1;
+		this.g.showDiceRollUI(first, second);
+		this.g.displayGameStateMessageUI("You rolled a " + first + " and a " + second + ".\nClick on the board to move! (one tile at a time)");
+		return first + second;
+	}
+
+	public boolean move(Location destination) {
+		if(destination.room == null || counter < 1){
+			if(counter < 1 || !g.getCanRoll()) {
+				g.displayGameStateMessageUI("You've used up all the dice roll moves");
+			}
+			return false;
+		}
+
+		// Ignore clicks within Rooms if a Players is currently in one
+		if(g.checkPlayerInRoom(this) && !destination.room.getName().equals("Passageway")){
+			return false;
+		}
+
+		Location currentLocation;
+		Location newLocation;
+
+		// First valid option is a player exiting a room
+		if (g.checkPlayerInRoom(this) && g.getPlayerRoom(this).getUnoccupiedExits().contains(destination)) {
+			g.movePlayer(this, destination);
+			counter--;
+			return true;
+		}
+
+		// Second valid option is a player entering a room
+		if(!destination.room.getName().equals("Passageway")
+				&& destination.room.getExits().contains(g.getPlayerLocation(this))){
+			g.movePlayer(this, destination.room.getRandomRoomLocation());
+			counter = 0;
+			g.enableSuggestion();
+			return true;
+		}
+
+		// Finally the third option is moving to an adjacent square
+		// in the passageway
+		else {
+			// Before anything, record the location we're moving from
+			currentLocation = g.getPlayerLocation(this);
+			String direction = currentLocation.checkAdjacent(destination);
+			if(direction.equals("Invalid")){
+				return false;
+			}
+			// Then first, check whether or not the move is valid
+			int moveAttemptResult = g.movePlayer(this, direction, locationsVisited, prevLocations);
+			if (moveAttemptResult == 0) {
+				// Successful move
+				newLocation = g.getPlayerLocation(this);
+				if (!prevLocations.isEmpty() && newLocation.equals(prevLocations.peek())) {
+					locationsVisited.remove(prevLocations.pop());
+					counter++;
+					return true;
+				} else {
+					locationsVisited.add(currentLocation);
+					prevLocations.push(currentLocation);
+					counter--;
+					return true;
+				}
+			} else if (moveAttemptResult < 0) {
+
+			}
+		}
+		return false;
+	}
+
+	public Set<Card> refuteV2(CardTuple tup){
+		Set<Card> refuteOptions = new HashSet<Card>();
+		for(Card c : hand.values()) {
+			if(tup.characterCard().equals(c)) { refuteOptions.add(c); }
+			if(tup.weaponCard().equals(c)) { refuteOptions.add(c);; }
+			if(tup.roomCard().equals(c)) { refuteOptions.add(c); }
+		}
+		return refuteOptions;
+	}
+
+	/**
+	 * prints the name of every card in players hand
+	 */
+	public void printCards() {
+		for(Card c : hand.values()) {
+			System.out.println(c.getName());
+		}
+	}
+
+	public String toString() { return "player " + playerNumber + " - username: " + playerName + ", character: " + characterName + ", in hand: " + hand.toString(); }
 }
